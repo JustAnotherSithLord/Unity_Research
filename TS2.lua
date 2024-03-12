3e2701734a5533c5655fcd4c4f191c626e1c9db0
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-12 15:17:33 -0500

    Update TS2.lua

--- TS2.lua
+++ TS2.lua
@@ -1,1 +1,11 @@
+# Thief Simulator 2
 
+AGGlobalSettings__.ctor	seg000	0000000000054A40	00000045			R	.	.	.	.	.	.	.
+
+AGGlobalSnowProps__.ctor	seg000	0000000000054E60	00000028			R	.	.	.	.	.	.	.
+
+AGGlobalSnow__.ctor	seg000	0000000000054C10	0000008E			R	.	.	.	.	.	.	.
+
+AGGlobalWind__.ctor	seg000	0000000000055070	00000066			R	.	.	.	.	.	.	.
+
+AI__.ctor	seg000	00000000000733D0	00000186			R	.	.	.	.	.	.	.
