4790e3458668d3ac30cb7aa54f1baac67bf5bf0c
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-12 15:17:48 -0500

    Update TS2.lua

--- TS2.lua
+++ TS2.lua
@@ -9,3 +9,13 @@ AGGlobalSnow__.ctor	seg000	0000000000054C10	0000008E			R	.	.	.	.	.	.	.
 AGGlobalWind__.ctor	seg000	0000000000055070	00000066			R	.	.	.	.	.	.	.
 
 AI__.ctor	seg000	00000000000733D0	00000186			R	.	.	.	.	.	.	.
+
+AI__AllBoolsFalse	seg000	000000000006E710	000000CA			R	.	.	.	.	.	.	.
+
+AI__ApplyEvent	seg000	000000000006D660	0000041F			R	.	.	.	.	.	.	.
+
+AI__Discover_Breakin	seg000	000000000006EC70	000000B2			R	.	.	.	.	.	.	.
+
+AI__GuardAlert	seg000	000000000006E1D0	00000065			R	.	.	.	.	.	.	.
+
+AI__LoadVoices	seg000	000000000006DD20	000000CB			R	.	.	.	.	.	.	.
