1f74f60d7fb48ea6bbc094031b4e9662374db425
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-13 12:21:06 -0500

    Update TPH.lua

--- TPH.lua
+++ TPH.lua
@@ -1,1 +1,7 @@
 # Two Point Hospital
+
+AmbienceConfig__.ctor	seg000	00000000001BF4C0	00000026			R	.	.	.	.	.	.	.
+
+CharAttributeVisualisation__.ctor	seg000	00000000001E6E30	0000001D			R	.	.	.	.	.	.	.
+
+CharModuleAssets__.cctor	seg000	00000000001C0990	00000008			R	.	.	.	.	.	.	.
