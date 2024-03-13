8aa013118c61207aa6355064317781312f590e64
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-13 12:21:30 -0500

    Update TPH.lua

--- TPH.lua
+++ TPH.lua
@@ -11,3 +11,9 @@ CharModuleAssets__.ctor	seg000	00000000001C08B0	0000002E			R	.	.	.	.	.	.	.
 CharacterHappinessComponent__.ctor	seg000	0000000000000C90	0000001F			R	.	.	.	.	.	.	.
 
 ChildModuleChoice__.ctor	seg000	00000000001C0A00	00000012			R	.	.	.	.	.	.	.
+
+Clip__.ctor	seg000	00000000001BE140	0000001D			R	.	.	.	.	.	.	.
+
+ConfigData__.ctor	seg000	00000000001D15F0	0000004C			R	.	.	.	.	.	.	.
+
+ConfigData__.ctor_0	seg000	00000000001D7D80	00000053			R	.	.	.	.	.	.	.
