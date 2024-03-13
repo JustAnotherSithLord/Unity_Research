59c8d22a428e61494e7bf86fbc015fab613aafef
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-13 12:21:15 -0500

    Update TPH.lua

--- TPH.lua
+++ TPH.lua
@@ -5,3 +5,9 @@ AmbienceConfig__.ctor	seg000	00000000001BF4C0	00000026			R	.	.	.	.	.	.	.
 CharAttributeVisualisation__.ctor	seg000	00000000001E6E30	0000001D			R	.	.	.	.	.	.	.
 
 CharModuleAssets__.cctor	seg000	00000000001C0990	00000008			R	.	.	.	.	.	.	.
+
+CharModuleAssets__.ctor	seg000	00000000001C08B0	0000002E			R	.	.	.	.	.	.	.
+
+CharacterHappinessComponent__.ctor	seg000	0000000000000C90	0000001F			R	.	.	.	.	.	.	.
+
+ChildModuleChoice__.ctor	seg000	00000000001C0A00	00000012			R	.	.	.	.	.	.	.
